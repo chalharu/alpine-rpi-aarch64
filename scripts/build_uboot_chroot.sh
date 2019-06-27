7dc60bb542330e8532e94e8f60a4c5769a8e28e7
Mitsuharu Seki <mitsu1986@gmail.com>
2019-06-27 20:29:44 +0900

    change kernel 5.2 to 4.19

--- scripts/build_uboot_chroot.sh
+++ scripts/build_uboot_chroot.sh
@@ -7,6 +7,8 @@ export CROSS_COMPILE="ccache aarch64-linux-gnu-"
 export ARCH=arm64
 export LOCALVERSION=
 export CCACHE_DIR=/ccache
+export CFLAGS="-pipe -O3 -mcpu=cortex-a53 -funsafe-math-optimizations"
+export LDFLAGS="-pipe -O3 -mcpu=cortex-a53 -funsafe-math-optimizations"
 
 cd $BUILDDIR/u-boot
 make rpi_3_defconfig && make
